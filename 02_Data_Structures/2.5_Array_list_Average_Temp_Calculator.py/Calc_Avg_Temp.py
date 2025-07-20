e58a6aee622013ff6b744d2bb8842648f3c1ca46
Milind1234 <milindchavan525@gmail.com>
2025-07-20 19:07:00 +0530

    Data_Structures

--- 02_Data_Structures/2.5_Array_list_Average_Temp_Calculator.py/Calc_Avg_Temp.py
+++ 02_Data_Structures/2.5_Array_list_Average_Temp_Calculator.py/Calc_Avg_Temp.py
@@ -8,10 +8,13 @@ def Calc_Avg_Temp(temp_list, n):
     
     avg = sum(temp_list) // len(temp_list)
     print(f"Average Temperature: {avg}")
-    
+
+    count_above_avg = 0
     for idx, temp in enumerate(temp_list):
         if temp > avg:
             print(f"The temperature {temp} of day {idx+1} is above the avg temp {avg}.")
+            count_above_avg += 1
+    print(f"Total number of days above average temperature: {count_above_avg}")
 
 n = int(input("How many days you want to calculate avg temp for? "))
 Calc_Avg_Temp(my_list, n)
