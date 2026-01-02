b0c55615d6ae39f711fab992b568219e3320a892
Milind1234 <milindchavan525@gmail.com>
2026-01-02 20:06:26 +0530

    Data_Structures

--- 02_Data_Structures/14_Graph/07_Add_Edge.py
+++ 02_Data_Structures/14_Graph/07_Add_Edge.py
@@ -10,7 +10,46 @@ This note explains how to ADD EDGES to a graph using Python:
         ✔ print_graph() helper
         ✔ Time & Space Complexity
 
-This lecture continues from the previous "Add Vertex" lecture.
+This lectur'''
+Real-World Example: Multiprocessing for CPU-bound Tasks
+Scenario: Factorial Calculation
+Factorial calculations, especially for large numbers, 
+involve significant computational work. Multiprocessing 
+can be used to distribute the workload across multiple 
+CPU cores, improving performance.
+
+'''
+
+import multiprocessing
+import math
+import sys
+import time
+
+# Increase the maximum number of digits for integer conversion
+sys.set_int_max_str_digits(100000)
+
+## function to compute factorials of a given number 
+
+def computer_factorial(number):
+    print(f"Computing factorial of {number}")
+    result=math.factorial(number)
+    print(f"Factorial of {number} is {result}")
+    return result
+
+if __name__=="__main__":
+    numbers=[5000,6000,700,8000]
+
+    start_time=time.time()
+
+    ##create a pool of worker processes
+    with multiprocessing.Pool() as pool:
+        results=pool.map(computer_factorial,numbers)
+
+    end_time=time.time()
+
+    print(f"Results: {results}")
+    print(f"Time taken: {end_time - start_time} seconds")
+e continues from the previous "Add Vertex" lecture.
 """
 
 # =============================================================================
@@ -149,9 +188,8 @@ class Graph:
             ✔ O(1) per edge
         """
 
-        # IMPORTANT FIX:
-        # Your original condition  (vertex1 and vertex2) in dict.keys()  was incorrect.
-        # Correct check:
+        
+        # Edge Case:
         if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
             
             # Add edge to vertex1's list
